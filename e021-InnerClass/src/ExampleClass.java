8899460b569a0af50ea810cfb25df130053768e9
Daniel <danielromeroalaman@gmail.com>
2018-10-30 19:13:57 +0100

    e021-InnerClass

---
+++ e021-InnerClass/src/ExampleClass.java
@@ -0,0 +1,16 @@
+public class ExampleClass
+{
+    static int x=10;
+    int y=20;
+    private int z=30;
+
+    class cInner
+    {
+        void print()
+        {
+            System.out.println("x: " + x);
+            System.out.println("y: " + y);
+            System.out.println("z: " + z);
+        }
+    }
+}
---
+++ e021-InnerClass/src/Main.java
@@ -0,0 +1,27 @@
+/*
+    Author: Daniel Romero
+    Exercise:
+*/
+
+
+// Class Main
+public class Main
+{
+    // Methods
+    void init()
+    {
+        ExampleClass e = new ExampleClass();
+        ExampleClass.cInner i = e.new cInner();
+        i.print();
+
+        System.out.println("------------------- or ------------------");
+        
+        new ExampleClass().new cInner().print();
+    }
+
+    // Main method
+    public static void main (String args[])
+    {
+        new Main().init();
+    }
+}
