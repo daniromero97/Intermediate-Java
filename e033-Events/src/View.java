dce176ecfcb0191654f96ef529a8c4e9ab62b1d8
Daniel <danielromeroalaman@gmail.com>
2018-11-03 01:13:36 +0100

    e033-Events (incomplete)

---
+++ e033-Events/src/View.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class View extends JFrame
+{
+
+
+    public void init()
+    {
+        setTitle("Events");
+        setSize(500, 500);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        instances();
+        content();
+        config();
+    }
+
+    private void instances()
+    {
+
+    }
+
+    private void content()
+    {
+
+    }
+
+    private void config()
+    {
+
+    }
+}
